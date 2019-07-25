cacf6f17be70113784260898a4fe4549b5d20bb0
Traum <1690127128@qqcom>
2019-07-25 23:52:56 +0800

    update

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <?php if (!defined('DIR'))exit('非法访问'); ?>
 <br />
 <footer>
-Copyright &copy; 2019-2020 <a href="https://www.jysafe.cn/" target="_blank" >祭夜の咖啡馆</a>  All Rights Reserved. 
+Copyright &copy; 2019-<script>document.write(new Date().getFullYear())</script> <a href="https://www.jysafe.cn/" target="_blank" >祭夜の咖啡馆</a>  All Rights Reserved. 
 </footer>
 
 </body>
