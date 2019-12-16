2f9372cdc504e9b3985b213897ffd116af306244
Traum <jiyecafe@gmail.com>
2019-12-16 22:37:26 +0800

    数据整理，确定时间格式为时间戳

--- plugins/insert.php
+++ plugins/insert.php
@@ -8,6 +8,33 @@
 if (!defined('DIR'))
     exit('非法访问');
 
+/*
+//$content = file_get_contents(DIR.'/data/hitokoto - chinese.json');
+$content = file_get_contents(DIR . '/data/lp_hitokoto.json');
+
+$content = json_decode($content, true);
+//var_dump($content);
+//exit;
+foreach ($content as $key => $value) {
+    //echo $value['id'] . '--->' . $value['hitokoto'] . '<br />';
+    $array_sentence = array(
+        'content' => $value['hitokoto'],
+        'cat' => $value['cat'],
+        'catname' => $value['catname'],
+        'source' => $value['source'],
+        'author' => $value['author'],
+        'date' => $value['date'],
+        'user_id' => 0,
+    );
+    //exit($array_sentence['date']);
+    if(!check_hitokoto_similarity($value['hitokoto']))
+        add_hitokoto($array_sentence);
+    else
+        echo $value['id'] . '--->' . $value['hitokoto'] . '<br />';
+}
+
+exit;
+
 $file = DIR . "/data/hitokoto.txt";
 if (file_exists($file)) {
     $file = fopen($file, "r") or exit("Unable to open file!");
@@ -25,7 +52,7 @@ if (file_exists($file)) {
             'catname' => '来源网络-待补充',
             'source' => '来源网络-待补充',
             'author' => '来源网络-待补充',
-            'date' => '2019-08-12 00:00:00',
+            'date' => '1576505465',
             'user_id' => 0,
         );
         //exit($array_sentence['date']);
@@ -37,27 +64,5 @@ if (file_exists($file)) {
 }
 
 
-exit;
-
-//$content = file_get_contents(DIR.'/data/hitokoto - chinese.json');
-$content = file_get_contents(DIR . '/data/lp_hitokoto.json');
-
-$content = json_decode($content, true);
-//var_dump($content);
 //exit;
-foreach ($content as $key => $value) {
-    echo $value['id'] . '--->' . $value['hitokoto'] . '<br />';
-    # code...
-    $array_sentence = array(
-        'content' => $value['hitokoto'],
-        'cat' => $value['cat'],
-        'catname' => $value['catname'],
-        'source' => $value['source'],
-        'author' => $value['author'],
-        'date' => date('Y-m-d H:i:s', $value['date']),
-        'user_id' => 0,
-    );
-    //exit($array_sentence['date']);
-    if(!check_hitokoto_similarity($value['hitokoto']))
-    add_hitokoto($array_sentence);
-}
+*/
